2edd47f04928e24a07856545369877d625bea146
Plamen Dimitrov <plamen.d@protonmail.com>
2022-06-01 02:32:07 +0300

    steam api is busted, using gog instead

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+async function getSteamGameInfo() {
+  // example from the Steam API
+  // this produces a CORS error...
+  // const gameInfo = await fetch('https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=1466860&count=3&maxlength=300&format=json');
+  // const gameInfoAsJson = await gameInfo.json()
+  // const title = gameInfoAsJson.appnews.newsitems[0].title;
+  // console.log(title);
+}
+
+async function getGameInfo() {
+  // example from the GOG API
+  const gameInfo = await fetch('https://api.gog.com/v2/games/1207666353');
+  const gameInfoAsJson = await gameInfo.json()
+  const title = gameInfoAsJson._embedded.product.title;
+  console.log(title);
+}
+
 function App() {
+  getSteamGameInfo();
+  getGameInfo();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This will eventually be the Steam API site.
+          This will eventually be the Steam/GOG API site.
         </p>
       </header>
     </div>
