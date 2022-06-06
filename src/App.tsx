1838b5a856345df9288b55c2a47fee79273c198a
Plamen Dimitrov <plamen.d@protonmail.com>
2022-06-07 01:35:24 +0300

    better search

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Banner from './components/Banner';
 import data from './data'
 
 function App() {
+  const [searchText, setSearchText] = useState('');
   const [searchGames, setSearchGames] = useState(['']);
+  const [searchResultsVisible,setSearchResultsVisible] = useState(false);
   const inputBox = useRef<HTMLInputElement>(null);
   const searchGame = (text:string) => {
     if(text) {
@@ -15,7 +17,9 @@ function App() {
       })
     }
   }
-  const advancedSearchGame = (text:string) => {
+  const showListOfGames = (text:string) => {
+    setSearchText(text);
+    setSearchResultsVisible(text.length > 2)
     if(text.length > 2) {
       let arrayOfGameNames:string[] = [];
       data.forEach((el) => {
@@ -24,6 +28,7 @@ function App() {
         }
       })
       setSearchGames(arrayOfGameNames);
+      setSearchResultsVisible(arrayOfGameNames.length > 0);
     }
   }
 
@@ -66,15 +71,21 @@ function App() {
       <Banner />
     </section>
     <main>
-      <input type="search" name="search" id="search" onChange={(e) => {advancedSearchGame(e.target.value)}} ref={inputBox}/>
-      <button onClick={() => {searchGame(inputBox.current?.value as string)}}>Search</button>
-      {searchGames.slice(0,5).map((el, index) => {
-        return (
-          <p key={index}>{el}</p>
-        )
-      })}
-      
-
+      <section className='searchfield'>
+          <input type="search" name="search" id="search-input" placeholder='Search for a game' value={searchText} ref={inputBox} onChange={(e) => {showListOfGames(e.target.value)}}/>
+          <button id='search-btn' onClick={() => {searchGame(inputBox.current?.value as string)}}>Search</button>
+          { searchResultsVisible &&
+          <div className="search-results">
+            {searchGames.slice(0,5).map((el, index) => {
+              return (<>
+                <p className='single-search-result' key={index} onClick={()=> {setSearchText(el); setSearchResultsVisible(false)}}>{el}</p>
+                {index === searchGames.length - 1 || index === 4 ? null:<hr />}
+                {/* the above check removes the hr for the last element */}
+                </>
+                )
+              })}
+          </div>}
+      </section>
     </main>
     <footer>Design by <a href="http://w3layouts.com/">W3layouts</a></footer>
     </>
